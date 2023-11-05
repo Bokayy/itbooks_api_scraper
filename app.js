e34fd7af58ba6598cc8726181e4dbf2293b8d61b
Bokayy X <blabsy8@gmail.com>
2023-11-06 00:52:14 +0100

    must change the post to use  chunks

--- app.js
+++ app.js
@@ -2,24 +2,59 @@
 import https from 'node:https';
 
 let rawData = '';
-let humanReadable = '';
+let humanReadable = ''; //just json
 
-https.get('https://api.itbook.store/1.0/search/MongoDB', (res) => {
-  //console.log('statusCode:', res.statusCode);
-  //console.log('headers:', res.headers);
-
+/* https.get('https://api.itbook.store/1.0/search/MongoDB', (res) => {
   res.on('data', (chunk) => {
-    //process.stdout.write(chunk);
     rawData += chunk;
-    //console.log(chunk);
   });
-
   res.on('end', () => {
-    //console.log(JSON.parse(rawData));
     humanReadable = JSON.parse(rawData);
-    console.log(humanReadable);
+    console.log("line 20:\n", humanReadable);
   })
-
 }).on('error', (e) => {
   console.error(e);
-}); 
+});  */
+
+const postData= {
+  title: 'test',
+  subtitle: 'test',
+  isbn13: 'test',
+  price: 'test',
+  image: 'test',
+  url: 'test',
+};
+
+const postOptions = {
+  hostname: 'localhost',
+  port: 8080,
+  path:'/insert',
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/x-www-form-urlencoded',
+    'Content-Length': Buffer.byteLength(JSON.stringify(postData))
+  },
+};
+
+
+
+const postReq = https.request(postOptions, (res) => {
+  console.log('statusCode:', res.statusCode);
+  console.log('headers:', res.headers);
+
+  res.on('data', (d)=>{
+    console.log(d);
+  });
+});
+
+postReq.on('error', (e) =>{
+  console.error(e);
+});
+
+postReq.write(postData);
+req.end();
+
+//todo šalji na backendov localhost:port kombo-
+//todo - na JSONtoDB funkciju.
+//solution - https post
+
