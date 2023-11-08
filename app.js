06139626a29d8f3034dccf3e78c53c14a82f59af
Bokayy X <blabsy8@gmail.com>
2023-11-08 03:44:03 +0100

    not sure

--- app.js
+++ app.js
@@ -12,6 +12,23 @@ import http from 'node:http';
   console.error(e);
 });  */
 
+function makeHttpGetRequest() {
+  let rawData = '';
+  let humanReadable = '';
+  http.get('http://localhost:2339/books', (res) => {
+  res.on('data', (chunk) => {
+    rawData += chunk;
+  });
+  res.on('end', () => {
+    humanReadable = JSON.parse(rawData);
+    console.log("humanReadable: ", humanReadable);
+  })
+}).on('error', (e) => {
+  console.error(e);
+});
+return humanReadable;
+}
+
 
 function makeHttpRequest(){
 
@@ -27,12 +44,14 @@ function makeHttpRequest(){
   const postOptions = {
     protocol: 'http:',
     hostname: 'localhost',
-    port: 8080,
+    port: 2339,
     method: 'POST',
     path:'/insert',
     headers: {
       'Content-Type': 'application/json',
-      'Content-Length': Buffer.byteLength(JSON.stringify(postData)),
+      //'Content-Length': Buffer.byteLength(JSON.stringify(postData)),
+      'Accept': '*',
+      'User-Agent': 'Thunder Client (https://www.thunderclient.com)'
       //'Authorization': 'Basic TOKEN'
     },
   };
@@ -40,11 +59,9 @@ function makeHttpRequest(){
   return new Promise((resolve, reject) => {
     const req = http.request(postOptions, (res) => {
       let body = '';
-      
       res.on('data', (chunk)=>{
             body+=chunk;
           });
-  
       res.on('end', () => {
         console.log(body);
         if (res.statusCode / 2 === 100 ) {
@@ -56,16 +73,15 @@ function makeHttpRequest(){
             resolve('Failure');
         }
     });
-
     res.on('error', () => {
       console.log('error');
       reject(Error('HTTP call failed'));
     });
     req.write(JSON.stringify(body));
     req.end();
+  });
 });
-  })
-}
 
-makeHttpRequest();
+};
+makeHttpGetRequest();
 
