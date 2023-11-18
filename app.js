23cbf6040a68b14ec363585019b14eed1c52e441
Bokayy X <blabsy8@gmail.com>
2023-11-18 02:28:26 +0100

    refactored get with DRY principle, cleanup

--- app.js
+++ app.js
@@ -1,56 +1,42 @@
 //ES6 Module syntax 
-import http from 'node:http';
+import http, { get } from 'node:http';
 import https from 'node:https'; //for getting data from api.itbooks.store
 
 let url = `https://api.itbook.store/1.0/search/MongoDB`
 
-const getNumberOfBooks = (url,page) => new Promise((resolve,reject) =>{
-  if (!page){ //just give me the number of books
-    https.get(url, res => {
-      let getRequestData = '';
-      res.on('data', (chunk) => {
-        getRequestData += chunk;
-      });
-
-      res.on('end', ()=>{
-        console.log("data:", getRequestData);
-        if (getRequestData.length === 0){
-          reject(Error("data empty"));
-        }
-        resolve(JSON.parse(getRequestData).total); //resolve is most likely a Promise specific kind of return
-      });
-
-      res.on('error', error => {
-        reject(error);
-      });
-    });
+const getBooks = (url,page) => new Promise((resolve,reject) =>{
+  if (!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!page){
+    url += `/${page}`;
   }
-  else{
-    https.get(`${url}/${page}`, res => {
-      let getRequestData = '';
-        res.on('data', (chunk) => {
-          getRequestData += chunk;
-        });
+  https.get(url, res => {
+    let getRequestData = '';
 
-        res.on('end', ()=>{
-          resolve(JSON.parse(getRequestData).books); //resolve is most likely a Promise specific kind of return
-        });
+    res.on('data', (chunk) => {
+      getRequestData += chunk;
+    });
 
-        res.on('error', error => {
-          reject(error);
-        });
-      });
-    }
+    res.on('end', ()=>{
+      let json = JSON.parse(getRequestData);
+      if (!page) 
+        resolve(json.total); //just give me the number of books     
+      else
+        resolve(json.books); 
+    });
+
+    res.on('error', error => {
+      reject(error);
+    });
+  });
 });
 
 
 let booksArray = new Array();
 async function populateBooks() {
-  let totalNumberOfBooks = await getNumberOfBooks(url);
+  let totalNumberOfBooks = await getBooks(url);
   let numberOfNecessaryRequests = Math.ceil(totalNumberOfBooks / 10);
 
   for (let i = 1; i <= numberOfNecessaryRequests ; i++) {
-    let responseArray = await getNumberOfBooks(url,i);
+    let responseArray = await getBooks(url,i);
     booksArray.push(...responseArray);
   }
 }
@@ -83,22 +69,18 @@ function sendPostRequest(){
       res.on('end', (data) => {
         if (res.statusCode / 2 === 100 ) {
             console.log('Scraper: success');
-            console.log(body);
             resolve('Success'); //like return but for promises
             }
         else {
-            console.log('failed')
             console.log("failed: ", res.statusCode);
             resolve('Failure');
         }
     });
     res.on('error', () => {
-      console.log('error');
       reject(Error('HTTP call failed'));
     });
   });
   await populateBooks();
-  console.log(booksArray);
   if (booksArray.length === 0) {
     reject(Error('books array empty'));
   }
